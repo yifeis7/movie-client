0f9b1619eefee2a934f685e7a2915e61329ddabe
yifeis7 <yifeis7@illinois.edu>
2023-08-20 16:56:40 -0500

    merge

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 import React from 'react'
 
 const Layout = () => {
-    return (
-        <main>
-            <Outlet />
-        </main>
-    )
+  return (
+    <main>
+      <Outlet/>
+    </main>
+  )
 }
+
 export default Layout
