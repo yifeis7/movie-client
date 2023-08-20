b2756cc99155b2404ba1d762610644ec23339cfa
yifeis7 <yifeis7@illinois.edu>
2023-08-20 15:03:40 -0500

    temp(after welcome)

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,12 +1,33 @@
-import React from 'react'
 import './Hero.css';
 import Carousal from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 
 
-const Hero = () => {
+const Hero = ({movies}) => {
   return (
-    <div>Hero</div>
+    <div className = 'movie-carousel-container'>
+      <Carousal>
+        {
+          movies.map((movie) => {
+              return (
+                <Paper>
+                  <div className = 'movie-card-container'>
+					  <div className="movie-card">
+						  <div className="movie-poster">
+							  <img src={movie.poster} alt="" />
+						  </div>
+						  <div className = "movie-title">
+							  <h4>{movie.title}</h4>
+						  </div>
+					  </div>
+                  </div>
+                </Paper>
+              )
+          }
+          )
+        }
+      </Carousal>
+    </div>
   )
 }
 
